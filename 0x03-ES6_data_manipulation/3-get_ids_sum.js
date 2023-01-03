d168377c4fb06b68df4770ad162724f93bc67c85
Dianedylan <dianagatwiriikiamba@gmail.com>
2023-01-03 19:25:43 +0300

    3 compltd

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,5 @@
-export default function getStudentIdsSum(getListStudents) {
-  const myArray = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return myArray.reduce(((accumulator, myArray) => accumulator + myArray.id), 0);
+function getStudentIdsSum(studentList) {
+  return studentList.reduce(((accumulator, studentList) => accumulator + studentList.id), 0);
 }
+
+export default getStudentIdsSum;
