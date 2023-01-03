1e7fbe9746b863e490cb73c1eabf4f77f22ecf1d
Dianedylan <dianagatwiriikiamba@gmail.com>
2023-01-03 19:24:02 +0300

    typed err

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+function updateStudentGradeByCity(studentList, city, newGrades) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+  return studentList.filter((std) => std.location === city).map((std) => {
+    std.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    for (const grd of newGrades) {
+      if (grd.studentId === std.id) {
+        std.grade = grd.grade; // eslint-disable-line no-param-reassign
+      }
+    }
+    return std;
+  });
+}
+
+export default updateStudentGradeByCity;
